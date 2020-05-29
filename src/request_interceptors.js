60b2aef566b795467f43baac824a8ac857505e69
hoan <hoanngothanh@gmail.com>
2020-05-29 10:30:13 +0700

    update site

--- src/request_interceptors.js
+++ src/request_interceptors.js
@@ -34,6 +34,7 @@ const urlsList = [
   'https://robinhood.engineering/*',
   'https://blog.hipolabs.com/*',
   'https://ux.shopify.com/*',
+  'https://blog.thefuntasty.com/*',
 ];
 
 export default function intercept() {
